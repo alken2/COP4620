2bbb2ef3dbee8e4e26c8ed44f61fd321cd965baa
Kenny A <58489290+alken2@users.noreply.github.com>
2021-01-23 15:46:14 -0500

    Update Driver.java

--- Step 1/Driver.java
+++ Step 1/Driver.java
@@ -1,44 +1,45 @@
-import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
 
 public class Driver {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Parsing: " + args[0]);
 
-
-        String t = args[0];
-        CharStream input = CharStreams.fromFileName(t);
+        String filename = args[0];
+        System.out.println("Parsing: " + filename);
+        CharStream input = CharStreams.fromFileName(filename);
         Little lexer = new Little(input);
 
         Token token = lexer.nextToken();
         while (token.getType() != Little.EOF) {
-            System.out.println("Token Type: " + getTokenType(token.getType()) + "\nValue: " + token.getText());
+            System.out.println("\t" + getTokenType(token.getType()) + "\t\t" + token.getText());
             token = lexer.nextToken();
         }
     }
+
     private static String getTokenType(int tokenType) {
         switch (tokenType) {
-            case Little.OPEATORS:
-                return "OPERATORS";
-            case Little.COMMENT:
-                return "COMMENT";
             case Little.KEYWORD:
                 return "KEYWORD";
-            case Little.STRINGLITERAL:
-                return "STRINGLITERAL";
-            case Little.FLOATLITERAL:
-                return "FLOATLITERAL";
-            case Little.INTLITERAL:
-                return "INTLITERAL";
-            case Little.IDENTIFIER:
-                return "IDENTIFIER";
+            case Little.OPEATOR:
+                return "OPERATOR";
+            /*
+            case Little.COMMENT:
+                return "COMMENT";
             case Little.WS:
                 return "WS";
+            */
+            case Little.IDENTIFIER:
+                return "IDENTIFIER";
+            case Little.INTLITERAL:
+                return "INTLITERAL";
+            case Little.FLOATLITERAL:
+                return "FLOATLITERAL";
+            case Little.STRINGLITERAL:
+                return "STRINGLITERAL";
             default:
                 return "OTHER";
         }
     }
 }
-
