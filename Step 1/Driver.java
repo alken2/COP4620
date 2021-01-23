cd359f01b0d5e0c21abe931bf92713ef77553705
Josh Griffin <77467299+sortella@users.noreply.github.com>
2021-01-23 15:39:17 -0500

    Update Driver.java

--- Step 1/Driver.java
+++ Step 1/Driver.java
@@ -1,5 +1,44 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+
 public class Driver {
-    public static void main(String[] args) {
-        //TBD
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Parsing: " + args[0]);
+
+
+        String t = args[0];
+        CharStream input = CharStreams.fromFileName(t);
+        Little lexer = new Little(input);
+
+        Token token = lexer.nextToken();
+        while (token.getType() != Little.EOF) {
+            System.out.println("Token Type: " + getTokenType(token.getType()) + "\nValue: " + token.getText());
+            token = lexer.nextToken();
+        }
+    }
+    private static String getTokenType(int tokenType) {
+        switch (tokenType) {
+            case Little.OPEATORS:
+                return "OPERATORS";
+            case Little.COMMENT:
+                return "COMMENT";
+            case Little.KEYWORD:
+                return "KEYWORD";
+            case Little.STRINGLITERAL:
+                return "STRINGLITERAL";
+            case Little.FLOATLITERAL:
+                return "FLOATLITERAL";
+            case Little.INTLITERAL:
+                return "INTLITERAL";
+            case Little.IDENTIFIER:
+                return "IDENTIFIER";
+            case Little.WS:
+                return "WS";
+            default:
+                return "OTHER";
+        }
     }
 }
+
