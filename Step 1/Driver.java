17ed8032dfc82457fd02a8e1fe15dbf0bb337ae4
Josh Griffin <77467299+sortella@users.noreply.github.com>
2021-01-25 17:18:08 -0500

    Driver with file output.

--- Step 1/Driver.java
+++ Step 1/Driver.java
@@ -2,12 +2,16 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.Vocabulary;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Driver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String filename = args[0];
         CharStream input = null;
+        File outputs = new File("outputs.txt");
+        FileWriter myWriter = new FileWriter("outputs.txt");
         try {
             input = CharStreams.fromFileName(filename);
         }
@@ -16,12 +20,13 @@ public class Driver {
             System.exit(1);
         }
         Little lexer = new Little(input);
-        Token t = lexer.nextToken();
+        Token token = lexer.nextToken();
         Vocabulary v = lexer.getVocabulary();
-        while (t.getType() != Little.EOF) {
-            System.out.println("Token Type: " + v.getSymbolicName(t.getType()));
-            System.out.println("Value: "+ t.getText());
-            t = lexer.nextToken();
+        while (token.getType() != Little.EOF) {
+            myWriter.write("Token Type: " + v.getSymbolicName(token.getType()));
+            myWriter.write("\nValue: "+ token.getText()+ "\n");
+            token = lexer.nextToken();
         }
+        myWriter.close();
     }
 }
