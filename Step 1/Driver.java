748bf51c1a40ed25d07159d39a7c0dd75986ba8c
Kenny A <58489290+alken2@users.noreply.github.com>
2021-01-26 15:27:56 -0500

    Update Driver.java
    Performed code cleanup on driver test file, (hopefully) final version of Driver for Step 1

--- Step 1/Driver.java
+++ Step 1/Driver.java
@@ -1,25 +1,26 @@
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.Vocabulary;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import org.antlr.v4.runtime.*;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class Driver {
-    public static void main(String[] args) throws IOException {
-        String filename = args[0];
-        CharStream input = CharStreams.fromFileName(filename);
-        File outputs = new File("outputs.txt"); //never used
-        FileWriter writer = new FileWriter("outputs.txt");
+    public static void main(String[] args) throws Exception {
+        String inputfile = null;
+        ANTLRInputStream input;
+        InputStream is = System.in;
+        if (args.length > 0) {
+            inputfile = args[0];
+        }
+        if (inputfile != null) {
+            is = new FileInputStream(inputfile);
+        }
+        input = new ANTLRInputStream(is);
         Little lexer = new Little(input);
         Token t = lexer.nextToken();
         Vocabulary v = lexer.getVocabulary();
-        while (t.getType() != Little.EOF) {
-            writer.write("Token Type: " + v.getSymbolicName(t.getType()));
-            writer.write("\nValue: "+ t.getText()+ "\n");
+        while (t.getType() != t.EOF) {
+            System.out.println("Token Type: " + v.getSymbolicName(t.getType()));
+            System.out.println("Value: " + t.getText());
             t = lexer.nextToken();
         }
-        writer.close();
     }
 }
