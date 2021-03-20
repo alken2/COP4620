705f2053df56a45becdfebacd3bf709bc56f8702
Kenny A <58489290+alken2@users.noreply.github.com>
2021-03-20 14:24:38 -0400

    Upload Driver.java

---
+++ Driver.java
@@ -0,0 +1,43 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Driver {
+    public static void main(String[] args) throws Exception {
+        String inputfile = null;
+        ANTLRInputStream input;
+        InputStream is = System.in;
+        if (args.length > 0) {
+            inputfile = args[0];
+        }
+        if (inputfile != null) {
+            is = new FileInputStream(inputfile);
+        }
+
+        input = new ANTLRInputStream(is);
+        LittleLexer lexer = new LittleLexer(input);
+        CommonTokenStream to = new CommonTokenStream(lexer);
+        LittleParser parser = new LittleParser(to);
+
+        parser.removeErrorListeners();
+
+        Listener listener = new Listener();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        ParseTree tree = parser.program();
+        walker.walk(listener,tree);
+        LinkedHashMap map = (LinkedHashMap)listener.getSymbolTable();
+        Iterator it = map.entrySet().iterator();
+
+        for(Iterator i =it; i.hasNext();){
+            Map.Entry pair = (Map.Entry) it.next();
+            SymbolTable st = (SymbolTable) pair.getValue();
+            st.print();
+            it.remove();
+        }
+    }
+}
