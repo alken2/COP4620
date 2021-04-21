e01c9bafb9bb798e0d5c2e6ed8392a3765a98659
Kenny A <58489290+alken2@users.noreply.github.com>
2021-04-21 19:07:30 -0400

    Create ScopeNode.java

---
+++ Step 4/ScopeNode.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class ScopeNode extends AbstractNode {
+    //Data Field
+    private ArrayList<AbstractNode> children = new ArrayList<>();
+
+    //Constructors
+    ScopeNode(String element) {
+        super(element);
+    }
+
+    ScopeNode(String element, ArrayList<AbstractNode> children) {
+        super(element);
+        this.children = children;
+    }
+
+    //Methods
+    protected String getElement() {
+        return element;
+    }
+
+    public void print() {
+        System.out.println("Element: " + this.getElement());
+        for (int i = 0; i < children.size(); i++) {
+            System.out.println("Child node of " + this.getElement() + ": Index " + i);
+            this.getChildren(i).print();
+        }
+    }
+
+    public void addChild(AbstractNode child) {
+        this.children.add(child);
+    }
+
+    public AbstractNode getChildren(int index) {
+        return children.get(index);
+    }
+
+
+}
