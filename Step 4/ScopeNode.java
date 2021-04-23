ba8d8cc92136428fab6b904dc2509f91753a4fdf
Kenny A <58489290+alken2@users.noreply.github.com>
2021-04-23 00:11:17 -0400

    Update ScopeNode.java
    Added the parameters attribute and addParameters method

--- Step 4/ScopeNode.java
+++ Step 4/ScopeNode.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class ScopeNode extends AbstractNode {
     //Data Field
     private ArrayList<AbstractNode> children = new ArrayList<>();
+    private ArrayList<String> parameters = null;
 
     //Constructors
     ScopeNode(String element) {
@@ -31,13 +32,23 @@ public class ScopeNode extends AbstractNode {
     public void print() {
         System.out.println("Element: " + this.getElement());
         for (int i = 0; i < children.size(); i++) {
-            System.out.println("Child node of " + this.getElement() + ": Index " + i);
+            if (this.parameters != null) {
+                System.out.println("Child node of " + this.getElement() + this.parameters.toString() + ": Index " + i);
+            }
+            else {
+                System.out.println("Child node of " + this.getElement() + ": Index " + i);
+            }
             this.getChild(i).print();
         }
     }
 
     public void addChild(AbstractNode child) {
         this.children.add(child);
+        //System.out.println("Added child to " + this.getElement() + " <----> "  + child.getElement());
+    }
+
+    public void addParameters(ArrayList<String> slist) { //remember this method when dealing with function parameters
+        this.parameters = slist;
     }
 
     public int numChildren() {
