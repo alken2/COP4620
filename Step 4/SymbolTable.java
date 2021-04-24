4fc6b2f9bd00093eba4b0e47312020f9ded5533c
Kenny A <58489290+alken2@users.noreply.github.com>
2021-04-23 22:48:10 -0400

    Create SymbolTable.java

---
+++ Step 4/SymbolTable.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class SymbolTable {
+    private final String tableName;
+    private final LinkedHashMap<String, ArrayList<String>> st = new LinkedHashMap<>();
+
+    public SymbolTable(String tName) {
+        this.tableName = tName;
+    }
+
+    public void insert(String name, String type, String value) {
+        ArrayList<String> symbol = new ArrayList<>();
+        symbol.add(name);
+        symbol.add(type);
+        symbol.add(value);
+        st.put(name, symbol);
+    }
+
+    public boolean search(String tName) {
+        return st.get(tName) != null;
+    }
+
+    public ArrayList<String> getValue(String tName) {
+        if (search(tName)) {
+            return st.get(tName);
+        }
+        return null;
+    }
+
+    public void print() {
+        System.out.println("Symbol table " + tableName);
+        for (ArrayList<String> symbol : st.values()) {
+            String line = "name " + symbol.get(0) + " type " + symbol.get(1);
+            if (symbol.get(2) != null) {
+                line = line.concat(" value " + symbol.get(2));
+            }
+            System.out.println(line);
+        }
+        System.out.println();
+    }
+}
